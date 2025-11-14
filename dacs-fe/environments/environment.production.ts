817719f0adb7c336f921cf19e834f06f5ef86678
lucastonelotto1 <lucasfrantone@gmail.com>
2025-11-14 15:43:41 -0300

    feat: Add production and development environment configuration files for application settings

---
+++ dacs-fe/environments/environment.production.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    keycloak: {
+        url:  '/auth' ,
+        realm: 'dacs',
+        clientId: 'dacs-app'
+    },
+      backendForFrontendUrl: '/api'
+};
---
+++ dacs-fe/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    keycloak: {
+        url:  'http://localhost:8080/auth' ,
+        realm: 'dacs',
+        clientId: 'dacs-fe'
+    },
+      backendForFrontendUrl: 'http://localhost:9001/bff'
+};
